18504bef21551fc59d38efcea411b494029b91b4
Lacrizomiq <118858104+Lacrizomiq@users.noreply.github.com>
2024-05-31 16:10:33 +0200

    ajout route paramétré

--- myapp/controllers/mainControllers.go
+++ myapp/controllers/mainControllers.go
@@ -5,6 +5,9 @@ import (
 	"myapp/myapp/views"
 	"net/http"
 	"path/filepath"
+	"strconv"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func executeTemplate(w http.ResponseWriter, filename string, data interface{}) {
@@ -29,6 +32,32 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func ArticlePageHandler(w http.ResponseWriter, r *http.Request) {
+	articleIDStr := chi.URLParam(r, "id")
+	articleID, err := strconv.Atoi(articleIDStr)
+	if err != nil {
+		http.Error(w, "Invalid article ID", http.StatusBadRequest)
+		return
+	}
+
+	articles, err := models.LoadArticles("models/articles.json")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	var article models.Article
+	for _, a := range articles {
+		if a.ID == articleID {
+			article = a
+			break
+		}
+	}
+
+	if article.ID == 0 {
+		http.Error(w, "Article not found", http.StatusNotFound)
+		return
+	}
+
 	tplPath := filepath.Join("views/templates", "article.gohtml")
-	executeTemplate(w, tplPath, nil)
+	executeTemplate(w, tplPath, article)
 }
